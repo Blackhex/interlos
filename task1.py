6a348fcb4e3e1801c542560c1130863232de774e
Radek Bartoň <radek.barton@kiwi.com>
2017-12-10 15:57:06 +0100

    - Message.

--- task1.py
+++ task1.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 
-print("Hello World");
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("moose.ienze.me", 5123))
+
+def read():
+  line = ""
+  while not line.endswith('\n'):
+    line += s.recv(1)
+  print line
+  return line
+
+def send(t):
+  s.sendall(t +"\n")
+
+send("hraji membe")
+r = read()
+while not r.startswith("hraj"):
+  r = read()
+
+s.close()
+
